4a652ec40b082b15b253d03ed1f8f2393c5127d5
Santhi Raju <santhiraju32@gmail.com>
2024-07-25 12:22:51 +0530

    HomePage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,143 @@
 import Hero from "./Hero";
+import { Link } from "react-router-dom";
 
-const Home = () =>{
+
+const Home = ({movie}) =>{
+
+
     return(
         <>
             <Hero text="Welcome to Home Page.."/>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-8 offset-lg-2 my-5">
-                    <p className="lead">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                    </p>
+            <div className="container" style={{ display:'flex', justifyContent:'space-between' }}>
+
+                <div className="col-lg-2 col-md-4 col-4 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/db32LaOibwEliAmSL2jjDF6oDdj.jpg" className="card-img-top" alt='Star Wars: The Rise of Skywalker' />
+                        <div className="card-body">
+                            <h5 className="card-title">Star Wars: The Rise of Skywalker</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
                     </div>
                 </div>
+
+                <div className="col-lg-2 col-md-4 col-4 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/6DTcWoo8wWl2f2YPp21hfGAAD7O.jpg" className="card-img-top" alt='DeadTectives' />
+                        <div className="card-body">
+                            <h5 className="card-title">DeadTectives</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-2 col-md-4 col-4 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/btV7OWc6IbjrwZiB75klmgi3CsY.jpg" className="card-img-top" alt='To Your Last Death' />
+                        <div className="card-body">
+                            <h5 className="card-title">To Your Last Death</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-2 col-md-4 col-4 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/2bhI7Ivk7VmvUaJaDTxJUgOXp1E.jpg" className="card-img-top" alt='The Death & Life of John F. Donovan' />
+                        <div className="card-body">
+                            <h5 className="card-title">The Death & Life of John F. Donovan</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="container" style={{ display:'flex', justifyContent:'space-between' }}>
+
+                <div className="col-lg-2 col-md-3 col-2 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/qtutY89ku1m6i7tchJ3sUOQQBWd.jpg" className="card-img-top" alt='The Death of Antonio Sànchez Lomas' />
+                        <div className="card-body">
+                            <h5 className="card-title">The Death of Antonio Sànchez Lomas</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-2 col-md-3 col-2 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/r5O84lMJRMQStl8GGJzAgkkdqP0.jpg" className="card-img-top" alt='Le Daim' />
+                        <div className="card-body">
+                            <h5 className="card-title">Le Daim</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-2 col-md-3 col-2 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/loIRe96n7jq9cvjxBOFahDghuG.jpg" className="card-img-top" alt='Matar al dragón' />
+                        <div className="card-body">
+                            <h5 className="card-title">Matar al dragón</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-2 col-md-3 col-2 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/ev8PDIuKm2j3m3jWxw497zCLjzu.jpg" className="card-img-top" alt='少年的你' />
+                        <div className="card-body">
+                            <h5 className="card-title">少年的你</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="container" style={{ display:'flex', justifyContent:'space-between' }}>
+
+                <div className="col-lg-2 col-md-3 col-2 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/htVovYJE966Rk2TGiBrbG9g20Ne.jpg" className="card-img-top" alt='La Dernière Folie de Claire Darling' />
+                        <div className="card-body">
+                            <h5 className="card-title">La Dernière Folie de Claire Darling</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-2 col-md-3 col-2 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/8cKs4cPTAhAjjRAOjjXFbVnYc1l.jpg" className="card-img-top" alt='返校' />
+                        <div className="card-body">
+                            <h5 className="card-title">返校</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-2 col-md-3 col-2 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/nRmPaVH1wuKWk2A66xYORVr3Y7.jpg" className="card-img-top" alt='Il signor Diavolo' />
+                        <div className="card-body">
+                            <h5 className="card-title">Il signor Diavolo</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-lg-2 col-md-3 col-2 my-4 mx-4">
+                    <div className="card">
+                        <img src="https://image.tmdb.org/t/p/w500/jeclnbGov6lbHu8o1ugcOBCUBlU.jpg" className="card-img-top" alt='A pesar de todo' />
+                        <div className="card-body">
+                            <h5 className="card-title">A pesar de todo</h5>
+                            <Link to="/movies/181812" className="btn btn-primary">Show details</Link>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </>
     )
